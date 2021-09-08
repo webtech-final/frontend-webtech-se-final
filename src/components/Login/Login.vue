cc7cdc3b4932dd810ae56f6c0e48f5c6fc2fd9b7
Lapat <lapat.s@ku.th>
2021-09-08 21:53:44 +0700

    add Login Page

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1 style="font-size: 70px">LOGIN</h1>
+        <div style="margin-top: 50px">
+            <input type="text" name="username" placeholder="username" v-model="form.username">
+        </div>
+        <div style="margin-top: 50px;">
+            <input type="password" name="password" placeholder="password" v-model="form.password">
+        </div>
+        <div style="color: black; margin-top: 50px;">
+            <button style="background-color:white; padding: 3px 6px">LOGIN</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            form: {
+                username: '',
+                password: ''
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg">
+        <div style="padding-top: 200px"><login></login></div>
+    </div>
+</template>
+
+<script>
+import Login from '../components/Login/Login.vue'
+export default {
+    components:{
+        Login
+    }
+}
+</script>
+
+<style>
+.bg{
+    background-image: url('../assets/background.png');
+    background-position: center;
+    background-size: cover;
+    min-height: 100vh;
+}
+</style>
