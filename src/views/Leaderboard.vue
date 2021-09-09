7db60de659ed672fe8151576529764d768b52983
Lapat <lapat.s@ku.th>
2021-09-09 16:04:43 +0700

    add Leaderboard Page

---
+++ src/views/Leaderboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg" style="padding-top: 100px; padding-bottom: 100px">
+        <div><leaderboard></leaderboard></div>
+    </div>
+</template>
+
+<script>
+import Leaderboard from '../components/leaderboard/Leaderboard.vue'
+export default {
+    components:{
+        Leaderboard
+    }
+}
+</script>
+
+<style>
+.bg{
+    background-image: url('../assets/background.png');
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+    min-height: 100vh;
+}
+</style>
