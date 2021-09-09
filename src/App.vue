2bfa42acc037c4272886139ffe374aced3249c38
Lapat <lapat.s@ku.th>
2021-09-09 14:52:12 +0700

    add Register Page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link class="bg-red-400" to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+body{
+  margin: 0px;
+  overflow-x: hidden;
+  min-height: 100vh;
+}
 </style>
---
+++ src/views/Regist.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg">
+        <div><regist></regist></div>
+    </div>
+</template>
+
+<script>
+import Regist from '../components/Regist/Regist.vue'
+export default {
+    components:{
+        Regist
+    }
+}
+</script>
+
+<style>
+.bg{
+    background-image: url('../assets/background.png');
+    background-position: center;
+    background-size: cover;
+    min-height: 100vh;
+}
+</style>
