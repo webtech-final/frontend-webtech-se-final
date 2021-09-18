42e41a71f53542e2ef849fd47450eec8516d357d
bigbigbig <krisada.m@ku.th>
2021-09-18 22:30:10 +0700

    make EnterRoom component work properly

--- src/components/home/EnterRoom.vue
+++ src/components/home/EnterRoom.vue
@@ -1,70 +1,88 @@
 <template>
-    <div class="bg-gray-400 grid-cols-1 p-10">
-        <div class="text-center text-3xl m-2 mx-auto place-self-center">
-            <a class="text-center w-full">PLEASE ENTER ROOM PIN BELOW</a>
-        </div>
-        <div class="my-7 relative w-1/3  mx-auto">
-            <input
-                class="
-                    input
-                    border border-gray-400
-                    appearance-none
-                    rounded
-                    w-full
-                    px-3
-                    py-3
-                    pt-5
-                    pb-2
-                    focus focus:border-indigo-600 focus:outline-none
-                    active:outline-none active:border-indigo-600
-                "
-                id="email"
-                type="text"
-                autofocus
-            />
-            <label
-                for="email"
-                class="
-                    label
-                    absolute
-                    mb-0
-                    -mt-2
-                    pt-4
-                    pl-3
-                    leading-tighter
-                    text-gray-400 text-base
-                    mt-2
-                    cursor-text
-                "
-                >Email Address</label
-            >
-            <div class="text-center mt-6 -mb-3">
-                <button
-                    class="
-                        py-3
-                        px-6
-                        text-white
-                        rounded-lg
-                        bg-red-400
-                        shadow-lg
-                        block
-                        md:inline-block
-                    "
-                >
-                    CREATE
-                </button>
-            </div>
-        </div>
-    </div>
+	<div class="flex shadow-md">
+		<div
+			class="
+				grid-cols-1
+				p-10
+				border-2
+				bg-gradient-to-b
+				from-indigo-50
+				to-purple-300
+			"
+		>
+			<div class="text-center text-3xl m-2 mx-auto place-self-center">
+				<a class="text-center w-full">PLEASE ENTER ROOM PIN BELOW</a>
+			</div>
+			<div class="my-7 relative">
+				<input
+					class="
+						input
+						border border-gray-400
+						appearance-none
+						rounded
+						w-full
+						px-3
+						py-3
+						pt-5
+						pb-2
+						focus
+						focus:border-indigo-600
+						focus:outline-none
+						active:outline-none
+						active:border-indigo-600
+					"
+					id=""
+					type="text"
+					autofocus
+				/>
+				<label
+					for="email"
+					class="
+						label
+						absolute
+						mb-0
+						-mt-2
+						pt-4
+						pl-3
+						leading-tighter
+						text-gray-400 text-base
+						mt-2
+						cursor-text
+					"
+					>Room Pin</label
+				>
+			</div>
+			<div class="flex justify-center">
+				<button
+					class="
+						items-center
+						justify-center
+						py-3
+						px-6
+						text-white
+						rounded-lg
+						shadow-lg
+						block
+						md:inline-block
+					"
+					:class="msg === 'JOIN' ? 'bg-blue-400' : 'bg-red-400'"
+					@click="handleBtn"
+				>
+					{{ msg }}
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
-
 <script>
-export default {
-    name: 'EnterRoom',
-    props: {
-        msg: String,
-    },
-}
+	export default {
+		name: "EnterRoom",
+		props: ["msg"],
+		methods: {
+			handleBtn() {
+				this.$emit("onHome", this.msg);
+			},
+		},
+	};
 </script>
-
-<style lang='scss' scoped></style>
+<style lang="scss" scoped></style>
