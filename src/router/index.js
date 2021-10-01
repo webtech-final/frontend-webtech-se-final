f4830f6a5f68a41b41296ee1ee8a25787448059f
Nataset Tanabodee <nataset.t@ku.th>
2021-10-01 18:58:47 +0700

    edit index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,76 +1,70 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Profile from '@/views/Profile.vue'
-import Leaderboard from '../views/Leaderboard'
-import Regist from '../views/Regist.vue'
-import Inventory from "../views/Inventory.vue";
-import Shop from "../views/Shop.vue";
-import Single from "../views/Singleplayer.vue"
-import Multi from "../views/Multiplayer.vue"
-import Test from "../views/Test.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import Profile from '@/views/Profile.vue';
+import Leaderboard from '../views/Leaderboard';
+import Regist from '../views/Regist.vue';
+import Inventory from '../views/Inventory.vue';
+import Shop from '../views/Shop.vue';
+import Single from '../views/Singleplayer.vue';
+import Multi from '../views/Multiplayer.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile,
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile,
+    },
 
-  {
-    path: "/leaderboard",
-    name: "Leaderboard",
-    component: Leaderboard,
-  },
-  {
-    path: "/regist",
-    name: "Regist",
-    component: Regist,
-  },
-  {
-    path: "/inventory",
-    name: "Inventory",
-    component: Inventory,
-  },
-  {
-    path: "/shop",
-    name: "Shop",
-    component: Shop,
-  },
-  {
-    path: "/single",
-    name: "Single",
-    component: Single
-  },
-  {
-    path: "/multi",
-    name: "Multi",
-    component: Multi 
-  },
-  {
-    path: "/test",
-    name: "Test",
-    component: Test 
-  }
+    {
+        path: '/leaderboard',
+        name: 'Leaderboard',
+        component: Leaderboard,
+    },
+    {
+        path: '/regist',
+        name: 'Regist',
+        component: Regist,
+    },
+    {
+        path: '/inventory',
+        name: 'Inventory',
+        component: Inventory,
+    },
+    {
+        path: '/shop',
+        name: 'Shop',
+        component: Shop,
+    },
+    {
+        path: '/single',
+        name: 'Single',
+        component: Single,
+    },
+    {
+        path: '/multi',
+        name: 'Multi',
+        component: Multi,
+    },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
